9541e5594e70d3459f221dc656216d0294da8a44
Ryan Mahoney <ryan@virtuecenter.com>
2014-12-24 14:19:10 -0500

    coding style

--- src/Interfaces.php
+++ src/Interfaces.php
@@ -23,94 +23,104 @@
  * THE SOFTWARE.
  */
 namespace Opine\Interfaces;
+
 use Closure;
 use ArrayObject;
 use MongoCursor;
 
-interface Cache {
-    public function set ($key, $value, $expire);
-    public function get ($key);
-    public function delete ($key, $timeout);
-    public function getSetGet ($key, Closure $callback, $ttl);
-    public function getSetGetBatch (Array &$items, $ttl);
-    public function getBatch (Array &$items);
-    public function deleteBatch (Array $items, $timeout);
+interface Cache
+{
+    public function set($key, $value, $expire);
+    public function get($key);
+    public function delete($key, $timeout);
+    public function getSetGet($key, Closure $callback, $ttl);
+    public function getSetGetBatch(Array &$items, $ttl);
+    public function getBatch(Array &$items);
+    public function deleteBatch(Array $items, $timeout);
 }
 
-interface Config {
-    public function get ($key);
+interface Config
+{
+    public function get($key);
 }
 
-interface Container {
-    public function set ($serviceName, $value, $scope, Array $arguments, Array $calls);
-    public function get ($serviceName);
+interface Container
+{
+    public function set($serviceName, $value, $scope, Array $arguments, Array $calls);
+    public function get($serviceName);
 }
 
-interface DB {
-    public function userIdSet ($userId);
-    public function collectionList ($system);
-    public function collection ($collection);
-    public function each (MongoCursor $cursor, Closure $callback);
-    public function id ($id);
-    public function mapReduce ($map, $reduce, Array $command, &$response, $fetch);
-    public function document ($dbURI, $document);
-    public function distinct ($collection, $key, Array $query);
-    public function fetchAllGrouped (MongoCursor $cursor, $key, $value);
-    public function date ($dateString);
+interface DB
+{
+    public function userIdSet($userId);
+    public function collectionList($system);
+    public function collection($collection);
+    public function each(MongoCursor $cursor, Closure $callback);
+    public function id($id);
+    public function mapReduce($map, $reduce, Array $command, &$response, $fetch);
+    public function document($dbURI, $document);
+    public function distinct($collection, $key, Array $query);
+    public function fetchAllGrouped(MongoCursor $cursor, $key, $value);
+    public function date($dateString);
 }
 
-interface DBDocument {
-    public function increment ($field, $value);
-    public function decrement ($field, $value);
-    public function upsert (Array $document);
-    public function remove ();
-    public function current ();
-    public function collection ();
-    public function id ();
-    public function get ($field);
-    public function checkByCriteria (Array $criteria);
+interface DBDocument
+{
+    public function increment($field, $value);
+    public function decrement($field, $value);
+    public function upsert(Array $document);
+    public function remove();
+    public function current();
+    public function collection();
+    public function id();
+    public function get($field);
+    public function checkByCriteria(Array $criteria);
 }
 
-interface Topic {
-    public function subscribe ($topic, $callback);
-    public function publish ($topic, ArrayObject $context);
+interface Topic
+{
+    public function subscribe($topic, $callback);
+    public function publish($topic, ArrayObject $context);
 }
 
-interface Route {
-    public function pathGet ();
-    public function queryStringGet ();
-    public function getGet ();
-    public function before ($callback);
-    public function after ($callback);
+interface Route
+{
+    public function pathGet();
+    public function queryStringGet();
+    public function getGet();
+    public function before($callback);
+    public function after($callback);
     public function purgeAfter();
-    public function purgeBefore ();
-    public function get ($pattern, $callback);
-    public function post ($pattern, $callback);
-    public function delete ($pattern, $callback);
-    public function patch ($pattern, $callback);
-    public function put ($pattern, $callback);
-    public function show ();
-    public function execute (Array $callable, Array $parameters, Array $beforeActionsIn, Array $afterActionsIn);
-    public function run ();
-    public function runNamed ($name);
-    public function namedRoutesGet ();
-    public function redirect ();
-    public function service ($name);
-    public function serviceMethod ($compositeName);
+    public function purgeBefore();
+    public function get($pattern, $callback);
+    public function post($pattern, $callback);
+    public function delete($pattern, $callback);
+    public function patch($pattern, $callback);
+    public function put($pattern, $callback);
+    public function show();
+    public function execute(Array $callable, Array $parameters, Array $beforeActionsIn, Array $afterActionsIn);
+    public function run();
+    public function runNamed($name);
+    public function namedRoutesGet();
+    public function redirect();
+    public function service($name);
+    public function serviceMethod($compositeName);
 }
 
-interface Layout {
-    public function make ($config, $container, Array $context, $debug);
-    public function config ($paths, Array $context, $debug);
-    public function container ($paths, Array $context, $debug);
+interface Layout
+{
+    public function make($config, $container, Array $context, $debug);
+    public function config($paths, Array $context, $debug);
+    public function container($paths, Array $context, $debug);
 }
 
-interface LayoutContainer {
-    public function region ($id, Array $region);
-    public function url ($id, $url);
-    public function args ($id, Array $args);
-    public function partial ($id, $partial);
-    public function data ($id, $data, $type);
-    public function write ();
-    public function render ($mode);
+interface LayoutContainer
+{
+    public function region($id, Array $region);
+    public function url($id, $url);
+    public function args($id, Array $args);
+    public function partial($id, $partial);
+    public function data($id, $data, $type);
+    public function write();
+    public function render($mode);
 }
